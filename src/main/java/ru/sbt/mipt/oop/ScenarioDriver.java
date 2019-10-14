b49473b3e167badef545db9c1592386b6b9f6d5a
katenkar23 <katenkar.ea@mail.ru>
2019-10-14 16:45:36 +0300

    Change ScenarioDriver class

--- src/main/java/ru/sbt/mipt/oop/ScenarioDriver.java
+++ src/main/java/ru/sbt/mipt/oop/ScenarioDriver.java
@@ -1,11 +1,15 @@
 package ru.sbt.mipt.oop;
 
 // Интерфейс обработчика сценариев умного дома
-public interface ScenarioDriver {
+public class ScenarioDriver {
 
     // Метод обработки сценария закрытия входной двери холла
-    void runCloseHallDoorScenario();
+    void runCloseHallDoorScenario() {
+
+    }
 
     // Метод обработки сценария выключения всего света в доме
-    void runAllLightsOffScenario();
+    void runAllLightsOffScenario() {
+
+    }
 }
--- src/main/java/ru/sbt/mipt/oop/SensorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/SensorEventHandler.java
@@ -1,5 +1,6 @@
 package ru.sbt.mipt.oop;
 
+// Обработчик событий сенсоров
 public interface SensorEventHandler {
 
     void handle();
