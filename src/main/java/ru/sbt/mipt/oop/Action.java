a3340a1bb1bca8c713771363945ca5d074cf6478
katenkar23 <katenkar.ea@mail.ru>
2019-10-17 14:15:42 +0300

    Add Actionable interface & Action class

--- src/main/java/ru/sbt/mipt/oop/Action.java
+++ src/main/java/ru/sbt/mipt/oop/Action.java
@@ -1,14 +1,4 @@
 package ru.sbt.mipt.oop;
 
 public class Action {
-
-    private SensorEvent event;
-
-    public Action(SensorEvent event) {
-        this.event = event;
-    }
-
-    SensorEvent getEvent() {
-        return event;
-    }
 }
--- src/main/java/ru/sbt/mipt/oop/smarthome/SmartHome.java
+++ src/main/java/ru/sbt/mipt/oop/smarthome/SmartHome.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class SmartHome implements Actionable {
-
     Collection<Room> rooms;
 
     public SmartHome() {
@@ -21,19 +20,5 @@ public class SmartHome implements Actionable {
 
     public Collection<Room> getRooms() {
         return rooms;
-    }
-
-    @Override
-    public void execute(Action action) {
-        for (Room room : rooms) {
-            room.execute(action);
-
-            // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
-            // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-            if (room.getName().equals("hall") && action.getEvent().getType().equals(SensorEventType.DOOR_CLOSED)) {
-                ScenarioController scenarioController = new ScenarioController(this);
-                scenarioController.runCloseHallDoorScenario();
-            }
-        }
     }
 }
