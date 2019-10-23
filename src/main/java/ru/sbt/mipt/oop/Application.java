4c8bd2f6ec704de148034ebd4606dd9c08f9155f
katenkar23 <katenkar.ea@mail.ru>
2019-10-23 19:30:51 +0300

    Add EventController realisation

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -10,28 +10,12 @@ public class Application {
         JsonSmartHomeStateProvider smartHomeStateProvider = new JsonSmartHomeStateProvider("smart-home-1.js");
         SmartHome smartHome = smartHomeStateProvider.getHomeState();
 
-        // экземпляр конфигурационного класса
-        Setup setup = new Setup(smartHome);
+        EventController eventController = new EventController(smartHome);
 
-        // начинаем цикл обработки событий
-        SensorEvent event = getNextSensorEvent();
-        while (event != null) {
-
-            setup.handle(event);
-
-            event = getNextSensorEvent();
-        }
+        eventController.controlEvent();
     }
 
     public static void sendCommand(SensorCommand command) {
         System.out.println("Pretent we're sending command " + command);
-    }
-
-    private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
-        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
-        String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventType, objectId);
     }
 }
