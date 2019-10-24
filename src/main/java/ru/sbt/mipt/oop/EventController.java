5d22117bdc19714b94ec1272b7d23ba7611cfd15
katenkar23 <katenkar.ea@mail.ru>
2019-10-24 18:01:52 +0300

    Create EventGenerator class

--- src/main/java/ru/sbt/mipt/oop/EventController.java
+++ src/main/java/ru/sbt/mipt/oop/EventController.java
@@ -12,26 +12,18 @@ public class EventController {
         this.smartHome = smartHome;
     }
 
-    private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
-        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
-        String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventType, objectId);
-    }
-
     public void controlEvent() {
 
         // экземпляр конфигурационного класса
         Setup setup = new Setup(this.getSmartHome());
 
         // начинаем цикл обработки событий
-        SensorEvent event = getNextSensorEvent();
+        SensorEvent event = EventGenerator.getNextSensorEvent();
         while (event != null) {
 
             setup.handle(event);
 
-            event = getNextSensorEvent();
+            event = EventGenerator.getNextSensorEvent();
         }
     }
 }
