3ef196c47604720c4c9001e2b8aa27052e88dee2
katenkar23 <katenkar.ea@mail.ru>
2019-10-24 18:39:02 +0300

    Create DoorCommandController class

---
+++ src/main/java/ru/sbt/mipt/oop/DoorCommandController.java
@@ -0,0 +1,39 @@
+package ru.sbt.mipt.oop;
+
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_CLOSED;
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_OPEN;
+
+public class DoorCommandController implements HomeCommandController {
+
+    private final SmartHome smartHome;
+
+    public DoorCommandController(SmartHome smartHome) {
+        this.smartHome = smartHome;
+    }
+
+    @Override
+    public void action(SensorCommand command) {
+        if (command.getType() == CommandType.DOOR_OPEN || command.getType() == CommandType.DOOR_CLOSED) {
+            for (Room room : smartHome.getRooms()) {
+                for (Door door : room.getDoors()) {
+                    if (door.getId().equals(command.getObjectId())) {
+                        if (command.getType() == CommandType.DOOR_OPEN) {
+                            door.setOpen(true);
+                            System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
+                        } else {
+                            door.setOpen(false);
+                            System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
+                            // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
+                            // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
+                            if (room.getName().equals("hall")) {
+                                ScenarioController scenarioController = new ScenarioController(smartHome);
+                                scenarioController.runCloseHallDoorScenario();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
--- src/main/java/ru/sbt/mipt/oop/DoorSensorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/DoorSensorEventHandler.java
@@ -8,7 +8,7 @@ public class DoorSensorEventHandler implements SensorEventHandler {
     private final SmartHome smartHome;
 
     public DoorSensorEventHandler(SmartHome smarthome) {
-//        this.event = event;
+
         this.smartHome = smarthome;
     }
 
@@ -16,25 +16,11 @@ public class DoorSensorEventHandler implements SensorEventHandler {
     @Override
     public void handle(SensorEvent event) {
         if (event.getType() == DOOR_OPEN || event.getType() == DOOR_CLOSED) {
-            for (Room room : smartHome.getRooms()) {
-                for (Door door : room.getDoors()) {
-                    if (door.getId().equals(event.getObjectId())) {
-                        if (event.getType() == DOOR_OPEN) {
-                            door.setOpen(true);
-                            System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
-                        } else {
-                            door.setOpen(false);
-                            System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
-                            // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
-                            // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-                            if (room.getName().equals("hall")) {
-                                ScenarioController scenarioController = new ScenarioController(smartHome);
-                                scenarioController.runCloseHallDoorScenario();
-                            }
-                        }
-                    }
-                }
-            }
+            SensorCommand command = new SensorCommand(event);
+
+            DoorCommandController doorController = new DoorCommandController(smartHome);
+
+            doorController.action(command);
         }
     }
 }
