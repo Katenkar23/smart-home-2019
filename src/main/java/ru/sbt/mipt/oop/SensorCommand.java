b349a40df29aa9e40234a727a7090b74cd4de827
katenkar23 <katenkar.ea@mail.ru>
2019-10-24 18:39:02 +0300

    Create DoorCommandController class

--- src/main/java/ru/sbt/mipt/oop/SensorCommand.java
+++ src/main/java/ru/sbt/mipt/oop/SensorCommand.java
@@ -1,12 +1,32 @@
 package ru.sbt.mipt.oop;
 
 public class SensorCommand {
-    private final CommandType type;
+    private CommandType type;
     private final String objectId;
 
     public SensorCommand(CommandType type, String objectId) {
         this.type = type;
         this.objectId = objectId;
+    }
+
+    public SensorCommand(SensorEvent event) {
+        switch (event.getType()) {
+            case LIGHT_ON : this.type = CommandType.LIGHT_ON;
+            case LIGHT_OFF : this.type = CommandType.LIGHT_OFF;
+            case DOOR_OPEN : this.type = CommandType.DOOR_OPEN;
+            case DOOR_CLOSED : this.type = CommandType.DOOR_CLOSED;
+            default : this.type = CommandType.LIGHT_OFF;
+        }
+
+        this.objectId = event.getObjectId();
+    }
+
+    public CommandType getType() {
+        return type;
+    }
+
+    public String getObjectId() {
+        return objectId;
     }
 
     @Override
