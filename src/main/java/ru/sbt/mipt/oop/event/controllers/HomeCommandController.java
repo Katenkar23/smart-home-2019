bf20d2d124171646e228dc45c94c97bd4a55bf63
katenkar23 <katenkar.ea@mail.ru>
2019-10-31 15:00:48 +0300

    Add control method in HomeCommandController interface

--- src/main/java/ru/sbt/mipt/oop/event/controllers/HomeCommandController.java
+++ src/main/java/ru/sbt/mipt/oop/event/controllers/HomeCommandController.java
@@ -4,6 +4,8 @@ import ru.sbt.mipt.oop.event.SensorCommand;
 
 public interface HomeCommandController {
 
+    public void control(SensorCommand command);
+
     public static void sendCommand(SensorCommand command) {
         System.out.println("Pretent we're sending command " + command);
     }
