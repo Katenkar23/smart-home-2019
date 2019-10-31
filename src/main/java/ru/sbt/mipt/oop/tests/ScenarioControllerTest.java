8b3ab15946d4c358dbc5e77d545fe0e4babfbc1e
katenkar23 <katenkar.ea@mail.ru>
2019-10-31 09:34:22 +0300

    Some fixes after rebase

--- src/main/java/ru/sbt/mipt/oop/tests/ScenarioControllerTest.java
+++ src/main/java/ru/sbt/mipt/oop/tests/ScenarioControllerTest.java
@@ -5,11 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ScenarioControllerTest {
 
     @org.junit.jupiter.api.Test
-    void testRunCloseHallDoorScenario() {
-
-    }
-
-    @org.junit.jupiter.api.Test
     void testRunAllLightsOffScenario() {
     }
 }
