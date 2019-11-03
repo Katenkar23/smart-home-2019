829e940044828e9d88372438e11a43fb3c597a9d
Katenkar23 <Katenkar.EA@mail.ru>
2019-11-03 22:43:39 +0300

    Add tests

---
+++ src/main/java/ru/sbt/mipt/oop/event/handlers/LightEventHandlerTest.java
@@ -0,0 +1,10 @@
+package ru.sbt.mipt.oop.event.handlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LightEventHandlerTest {
+
+    @org.junit.jupiter.api.Test
+    void handle() {
+    }
+}
