65a0949622f5447f9fa1ed958cc4b786dff12eeb
Katenkar23 <Katenkar.EA@mail.ru>
2019-11-05 01:11:20 +0300

    Add AlarmSystem class

---
+++ src/main/java/ru/sbt/mipt/oop/smarthome/AlarmSystem.java
@@ -0,0 +1,11 @@
+package ru.sbt.mipt.oop.smarthome;
+
+import ru.sbt.mipt.oop.Action;
+import ru.sbt.mipt.oop.Actionable;
+
+public class AlarmSystem implements Actionable {
+    @Override
+    public void execute(Action action) {
+
+    }
+}
