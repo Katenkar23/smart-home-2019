6463d05a7dc84df5296c958782d66c01a5fd90c8
Katenkar23 <Katenkar.EA@mail.ru>
2019-11-05 01:39:30 +0300

    Add AlarmDeactivated class

---
+++ src/main/java/ru/sbt/mipt/oop/smarthome/alarm/AlarmDeactivated.java
@@ -0,0 +1,13 @@
+package ru.sbt.mipt.oop.smarthome.alarm;
+
+public class AlarmDeactivated implements AlarmSystemState {
+    @Override
+    public void activate(int code) {
+
+    }
+
+    @Override
+    public void deactivate(int code) {
+
+    }
+}
