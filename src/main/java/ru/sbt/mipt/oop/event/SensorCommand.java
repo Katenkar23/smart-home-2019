4d375b7d984329e0b44a2b4b17c02ba59ded7e62
katenkar23 <katenkar.ea@mail.ru>
2019-11-06 18:18:00 +0300

    Refactor SensorCommand class

--- src/main/java/ru/sbt/mipt/oop/event/SensorCommand.java
+++ src/main/java/ru/sbt/mipt/oop/event/SensorCommand.java
@@ -9,18 +9,6 @@ public class SensorCommand {
         this.objectId = objectId;
     }
 
-    public SensorCommand(SensorEvent event) {
-        switch (event.getType()) {
-            case LIGHT_ON : this.type = CommandType.LIGHT_ON;
-            case LIGHT_OFF : this.type = CommandType.LIGHT_OFF;
-            case DOOR_OPEN : this.type = CommandType.DOOR_OPEN;
-            case DOOR_CLOSED : this.type = CommandType.DOOR_CLOSED;
-            default : this.type = CommandType.LIGHT_OFF;
-        }
-
-        this.objectId = event.getObjectId();
-    }
-
     public CommandType getType() {
         return type;
     }
