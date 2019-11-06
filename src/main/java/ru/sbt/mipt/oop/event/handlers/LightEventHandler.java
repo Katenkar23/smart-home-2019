ff80fd7c1dd9dc78938552204c660ca4234e55ac
katenkar23 <katenkar.ea@mail.ru>
2019-11-06 20:05:29 +0300

    Delete Iterators, refactor DoorEventHandler & LightEventHandler

--- src/main/java/ru/sbt/mipt/oop/event/handlers/LightEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/event/handlers/LightEventHandler.java
@@ -1,8 +1,6 @@
 package ru.sbt.mipt.oop.event.handlers;
 
 import ru.sbt.mipt.oop.smarthome.Light;
-import ru.sbt.mipt.oop.smarthome.LightIterator;
-import ru.sbt.mipt.oop.smarthome.Room;
 import ru.sbt.mipt.oop.smarthome.SmartHome;
 import ru.sbt.mipt.oop.event.SensorEvent;
 
@@ -23,23 +21,20 @@ public class LightEventHandler implements SensorEventHandler {
     @Override
     public void handle(SensorEvent event) {
         if (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF) {
-
-            LightIterator lightIterator = new LightIterator(smartHome);
-
-            while (lightIterator.hasNext()) {
-
-                Light light = lightIterator.next();
-
-                if (light.getId().equals(event.getObjectId())) {
-                    if (event.getType() == LIGHT_ON) {
-                        light.setOn(true);
-                        System.out.println("Light " + light.getId() + " in room was opened.");
-                    } else {
-                        light.setOn(false);
-                        System.out.println("Light " + light.getId() + " in room was closed.");
+            smartHome.execute(actionable -> {
+                if (actionable instanceof Light) {
+                    Light light = (Light) actionable;
+                    if (light.getId().equals(event.getObjectId())) {
+                        if (event.getType() == LIGHT_ON) {
+                            light.setOn(true);
+                            System.out.println("Light " + light.getId() + " in room was opened.");
+                        } else {
+                            light.setOn(false);
+                            System.out.println("Light " + light.getId() + " in room was closed.");
+                        }
                     }
                 }
-            }
+            });
         }
     }
 }
--- src/main/java/ru/sbt/mipt/oop/scenarios/ScenarioController.java
+++ src/main/java/ru/sbt/mipt/oop/scenarios/ScenarioController.java
@@ -4,7 +4,6 @@ import ru.sbt.mipt.oop.event.CommandType;
 import ru.sbt.mipt.oop.event.SensorCommand;
 import ru.sbt.mipt.oop.event.CommandSender;
 import ru.sbt.mipt.oop.smarthome.Light;
-import ru.sbt.mipt.oop.smarthome.LightIterator;
 import ru.sbt.mipt.oop.smarthome.SmartHome;
 
 // Интерфейс обработчика сценариев умного дома
@@ -19,12 +18,7 @@ public class ScenarioController {
 
     // Метод обработки сценария выключения всего света в доме
     public void runAllLightsOffScenario() {
-
-        LightIterator lightIterator = new LightIterator(smartHome);
-
         while (lightIterator.hasNext()) {
-
-            Light light = lightIterator.next();
             light.setOn(false);
             SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
             System.out.println("Light " + light.getId() + " was turned off by HallDoor.");
