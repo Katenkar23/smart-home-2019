efc0e682809feb72bd7971b75aa3e411ba6ec69e
katenkar23 <katenkar.ea@mail.ru>
2019-11-19 13:43:54 +0300

    Add AlarmActivated implementation

--- src/main/java/ru/sbt/mipt/oop/smarthome/alarm/AlarmActivated.java
+++ src/main/java/ru/sbt/mipt/oop/smarthome/alarm/AlarmActivated.java
@@ -2,18 +2,29 @@ package ru.sbt.mipt.oop.smarthome.alarm;
 
 public class AlarmActivated implements AlarmSystemState {
 
+    private final AlarmSystem alarm;
+    private final String code;
+
+    public AlarmActivated(AlarmSystem alarm, String code) {
+        this.alarm = alarm;
+        this.code = code;
+    }
+
     @Override
     public void activate(String code) {
-
     }
 
     @Override
     public void deactivate(String code) {
-
+        if (this.code.equals(code)) {
+            alarm.setAlarmState(new AlarmDeactivated());
+        } else {
+            alarm.setAlarmState(new AlarmAlert());
+        }
     }
 
     @Override
     public void alert() {
-        
+        alarm.setAlarmState(new AlarmAlert());
     }
 }
