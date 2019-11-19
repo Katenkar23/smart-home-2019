cc2768f103414dac111c59d2770edcdad21b14c4
katenkar23 <katenkar.ea@mail.ru>
2019-11-19 13:53:21 +0300

    Add AlarmDeactivated implementation

--- src/main/java/ru/sbt/mipt/oop/smarthome/alarm/AlarmDeactivated.java
+++ src/main/java/ru/sbt/mipt/oop/smarthome/alarm/AlarmDeactivated.java
@@ -2,18 +2,23 @@ package ru.sbt.mipt.oop.smarthome.alarm;
 
 public class AlarmDeactivated implements AlarmSystemState {
 
+    private final AlarmSystem alarm;
+
+    public AlarmDeactivated(AlarmSystem alarm) {
+        this.alarm = alarm;
+    }
+
     @Override
     public void activate(String code) {
-
+        alarm.setAlarmState(new AlarmActivated(alarm, code));
     }
 
     @Override
     public void deactivate(String code) {
-
     }
 
     @Override
     public void alert() {
-
+        alarm.setAlarmState(new AlarmAlert());
     }
 }
