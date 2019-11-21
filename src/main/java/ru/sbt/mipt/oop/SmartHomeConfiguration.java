50b9e4217e98aa92d02bc1c4ee5296ecaba305ae
katenkar23 <katenkar.ea@mail.ru>
2019-11-21 19:09:22 +0300

    Add spring realisation

---
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeConfiguration.java
@@ -0,0 +1,35 @@
+package ru.sbt.mipt.oop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.sbt.mipt.oop.event.handlers.CCEventHandlerAdapter;
+import ru.sbt.mipt.oop.event.manager.SensorEventsManager;
+import ru.sbt.mipt.oop.smarthome.JsonSmartHomeStateProvider;
+import ru.sbt.mipt.oop.smarthome.SmartHome;
+import ru.sbt.mipt.oop.smarthome.alarm.AlarmSystem;
+
+@Configuration
+public class SmartHomeConfiguration {
+
+    @Bean
+    SmartHome smartHome() {
+        return new JsonSmartHomeStateProvider("smart-home-1.js").getHomeState();
+    }
+
+    @Bean
+    CCEventHandlerAdapter ccEventHandlerAdapter() {
+        return new CCEventHandlerAdapter(smartHome());
+    }
+
+    @Bean
+    AlarmSystem alarmSystem() {
+        return smartHome().getAlarm();
+    }
+
+    @Bean
+    SensorEventsManager sensorEventsManager() {
+        SensorEventsManager seManager = new SensorEventsManager();
+        seManager.registerEventHandler(ccEventHandlerAdapter());
+        return new SensorEventsManager();
+    }
+}
