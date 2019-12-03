1ed6972e663d8bfc8afe8506007c2c80552e2ff2
katenkar23 <katenkar.ea@mail.ru>
2019-12-03 17:33:05 +0300

    Refactor CCEventHandlerAdapter for Chain of Responsibility

--- src/main/java/ru/sbt/mipt/oop/event/handlers/CCEventHandlerAdapter.java
+++ src/main/java/ru/sbt/mipt/oop/event/handlers/CCEventHandlerAdapter.java
@@ -1,25 +1,19 @@
 package ru.sbt.mipt.oop.event.handlers;
 
 import ru.sbt.mipt.oop.event.SensorEvent;
-import ru.sbt.mipt.oop.event.SensorEventType;
 import ru.sbt.mipt.oop.event.manager.CCSensorEvent;
 import ru.sbt.mipt.oop.event.manager.EventHandler;
 
 import java.util.Collection;
-import java.util.HashMap;
 
 import static ru.sbt.mipt.oop.event.SensorEventType.*;
 
 public class CCEventHandlerAdapter implements EventHandler {
 
     private final Collection<SensorEventHandler> handlers;
-    private final HashMap<String, SensorEventType> eventMap;
 
     public CCEventHandlerAdapter(Collection<SensorEventHandler> handlers) {
         this.handlers = handlers;
-        this.eventMap = new HashMap<>();
-
-        setEventMap();
     }
 
     @Override
@@ -32,21 +26,89 @@ public class CCEventHandlerAdapter implements EventHandler {
         }
     }
 
-    private void setEventMap() {
-
-        //"LightIsOn", "LightIsOff", "DoorIsOpen", "DoorIsClosed", "DoorIsLocked", "DoorIsUnlocked"
-
-        eventMap.put("LightIsOn", LIGHT_ON);
-        eventMap.put("LightIsOff", LIGHT_OFF);
-        eventMap.put("DoorIsOpen", DOOR_OPEN);
-        eventMap.put("DoorIsClosed", DOOR_CLOSED);
+    private SensorEvent adaptSensorEvent(CCSensorEvent event) {
+        return new LigntOnTypeChooser(new LigntOffTypeChooser(new DoorOpenTypeChooser(new DoorClosedTypeChooser(new EmptyTypeChooser())))).adaptEvent(event);
     }
 
-    private SensorEvent adaptSensorEvent(CCSensorEvent event) {
-        if (eventMap.get(event.getEventType()) != null) {
-            return new SensorEvent(eventMap.get(event.getEventType()), event.getObjectId());
+    //"LightIsOn", "LightIsOff", "DoorIsOpen", "DoorIsClosed", "DoorIsLocked", "DoorIsUnlocked"
+
+    interface EventTypeChooser {
+        SensorEvent adaptEvent(CCSensorEvent ccEvent);
+    }
+
+    class LigntOnTypeChooser implements EventTypeChooser {
+        private final EventTypeChooser next;
+
+        LigntOnTypeChooser(EventTypeChooser next) {
+            this.next = next;
         }
 
-        return null;
+        @Override
+        public SensorEvent adaptEvent(CCSensorEvent ccEvent) {
+            if (ccEvent.getEventType().equals("LightIsOn")) {
+                return new SensorEvent(LIGHT_ON, ccEvent.getObjectId());
+            } else {
+                return next.adaptEvent(ccEvent);
+            }
+        }
+    }
+
+    class LigntOffTypeChooser implements EventTypeChooser {
+        private final EventTypeChooser next;
+
+        LigntOffTypeChooser(EventTypeChooser next) {
+            this.next = next;
+        }
+
+        @Override
+        public SensorEvent adaptEvent(CCSensorEvent ccEvent) {
+            if (ccEvent.getEventType().equals("LightIsOff")) {
+                return new SensorEvent(LIGHT_OFF, ccEvent.getObjectId());
+            } else {
+                return next.adaptEvent(ccEvent);
+            }
+        }
+    }
+
+    class DoorOpenTypeChooser implements EventTypeChooser {
+        private final EventTypeChooser next;
+
+        DoorOpenTypeChooser(EventTypeChooser next) {
+            this.next = next;
+        }
+
+        @Override
+        public SensorEvent adaptEvent(CCSensorEvent ccEvent) {
+            if (ccEvent.getEventType().equals("DoorIsOpen")) {
+                return new SensorEvent(DOOR_OPEN, ccEvent.getObjectId());
+            } else {
+                return next.adaptEvent(ccEvent);
+            }
+        }
+    }
+
+    class DoorClosedTypeChooser implements EventTypeChooser {
+        private final EventTypeChooser next;
+
+        DoorClosedTypeChooser(EventTypeChooser next) {
+            this.next = next;
+        }
+
+        @Override
+        public SensorEvent adaptEvent(CCSensorEvent ccEvent) {
+            if (ccEvent.getEventType().equals("DoorIsClosed")) {
+                return new SensorEvent(DOOR_CLOSED, ccEvent.getObjectId());
+            } else {
+                return next.adaptEvent(ccEvent);
+            }
+        }
+    }
+
+    class EmptyTypeChooser implements EventTypeChooser {
+
+        @Override
+        public SensorEvent adaptEvent(CCSensorEvent ccEvent) {
+            return null;
+        }
     }
 }
